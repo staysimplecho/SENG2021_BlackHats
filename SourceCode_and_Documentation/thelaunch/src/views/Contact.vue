b55eef6d96df39ef5fdaa927a1008bd9a5b7efc9
Meilin Huang <meilin@192-168-1-10.tpgi.com.au>
2020-04-16 22:38:21 +1000

    update

---
+++ SourceCode_and_Documentation/thelaunch/src/views/Contact.vue
@@ -0,0 +1,292 @@
+<template>
+  <div>
+    <div id="contact-wrap">
+      <Header></Header>
+    </div>
+    <section style="left:0;width:100%;min-width:980px;height:auto;top:;bottom:;right:;position:;margin-left:0" data-responsive="true" data-is-screen-width="true" data-col-margin="0" data-row-margin="0" class="strc1" id="comp-j9sf5fvi">
+      <div id="contact-middle">
+        <div>
+          <div id="comp-j9sf5fw9inlineContent-gridContainer">
+            
+            /*inlineContent of "AND LOCATION" box*/
+            <div style="top:;bottom:;left:;right:;position:" id="comp-jckajvku">
+              <div class="style-jhkmf1umbg">
+              </div>
+              <div class="style-jhkmf1uminlineContent" id="comp-jckajvkuinlineContent">
+                <div id="comp-jckajvkuinlineContent-gridWrapper">
+                  <div id="comp-jckajvkuinlineContent-gridContainer">
+                    <div data-svg-id="06f469158f5346a3be7e4100e38370cd.svg" data-svg-type="shape" data-display-mode="stretch" data-strokewidth="0" data-viewbox="0 0 200 200" data-preserve-viewbox="ignore" style="top:;bottom:;left:;right:;width:105px;height:7px;position:" class="style-jckajvkw" id="comp-jckajvkv">
+                      <div class="style-jckajvkwlink">
+                        <div style="stroke-width:0;fill-opacity:1;stroke:rgba(255, 255, 255, 1);stroke-opacity:1;fill:rgba(255, 255, 255, 1)" class="style-jckajvkw_non-scaling-stroke style-jckajvkwsvg">
+                          <svg preserveAspectRatio="none" data-bbox="17.3 68.2 165.3 63.6" xmlns="http://www.w3.org/2000/svg" viewBox="17.3 68.2 165.3 63.6" role="img">
+                            <g>
+                              <path d="M182.6 68.2v63.6H17.3V68.2h165.3z"/>
+                            </g>
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            /*inlineContent of "CONTACT box"*/
+            <div style="top:;bottom:;left:;right:;position:" id="comp-jckajvjq">
+              <div class="white-box">
+              </div>
+              <div class="style-jomyp1cpinlineContent" id="comp-jckajvjqinlineContent">
+                <div id="comp-jckajvjqinlineContent-gridContainer">
+                </div>
+              </div>
+            </div>
+
+            /*contact and location text*/
+            <div data-packed="false" data-min-height="55" class="txtNew" id="comp-jckajvkz" style="width: 515px; min-height: 55px; pointer-events: none;">
+              <h1 class="font_2">
+                <span class="color_11">
+                  <span style="letter-spacing:0.04em;">CONTACT AND LOCATION</span>
+                </span>
+              </h1>
+            </div>
+
+            /*black tilt line*/
+            <div data-svg-id="0dc8050383f4475f867f63ef8f5f2599.svg" data-svg-type="shape" data-display-mode="stretch" data-strokewidth="0" data-viewbox="0 0 200 200" data-preserve-viewbox="ignore" style="top:;bottom:;left:;right:;width:22px;height:52px;position:" class="style-jckajvkw" id="comp-jmstz2a3">
+              <div class="style-jckajvkwlink">
+                <div style="stroke-width:0;fill-opacity:1;stroke:rgba(252, 151, 121, 1);stroke-opacity:0;fill:rgba(252, 151, 121, 1)" class="style-jckajvkw_comp-jmstz2a3 style-jckajvkw_non-scaling-stroke style-jckajvkwsvg" id="comp-jmstz2a3svg">
+                  <svg preserveAspectRatio="none" data-bbox="61.9 10.1 76.2 179.8" viewBox="61.9 10.1 76.2 179.8" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="img">
+                    <g>
+                      <path d="M62 174.1l8.8 15.8h-4l-4.9-8.7v-7.1h.1zm0-11.1l14.9 26.8h4l-18.9-34v7.2zm0-18.1l25 45h4l-29-52.1v7.1zm0-18.1l35.1 63.1h4L62 119.7v7.1zm0-18.2l45.2 81.2h4L62 101.5v7.1zm0-18.1l55.3 99.4h4L62 83.4v7.1zm0-18.1l65.4 117.5h4L62 65.3v7.1zm0-18.2l75.5 135.6h.6v-6.1L62 47.1v7.1zm0-18.1l76 136.6v-7.1L62 29v7.1zM62 18l76 136.6v-7.1L62 10.9V18zm5.7-7.9L138 136.5v-7.1L71.6 10.1h-3.9zm10.1 0L138 118.3v-7.1L81.7 10.1h-3.9zm10.1 0l50.2 90.1v-7.1l-46.2-83h-4zm10.1 0l40.1 72V75L102 10.1h-4zm10.1 0L138 64v-7.1l-26-46.7h-3.9v-.1zm10.1 0L138 45.8v-7.1l-15.9-28.6h-3.9zm14 0h-4l9.8 17.6v-7.1l-5.8-10.5z"/>
+                    </g>
+                </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "Contact",
+  components: {
+    Header,
+  },
+};
+
+</script>
+
+<style lang="scss">
+// Your AWESOME styles go here
+#contact-wrap {
+  background-color: black;
+}
+
+#contact-middle {
+  background-color: black;
+  width: 100%;
+  position: relative;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+#comp-j9sf5fw9inlineContent-gridContainer {
+    position: static;
+    display: grid;
+    height: auto;
+    width: 100%;
+    min-height: 1262px;
+    grid-template-rows: min-content min-content min-content min-content min-content min-content min-content min-content min-content 1fr;
+    grid-template-columns: 100%;
+}
+
+#comp-jckajvjq {
+    position: relative;
+    margin: 64px 0px 10px calc((100% - 980px) * 0.5);
+    left: 8px;
+    grid-area: 1 / 1 / 2 / 2;
+    justify-self: start;
+    align-self: start;
+}
+
+#comp-jckajvkz {
+    position: relative;
+    margin: 69px 0px 10px calc((100% - 980px) * 0.5);
+    left: 41px;
+    grid-area: 1 / 1 / 2 / 2;
+    justify-self: start;
+    align-self: start;
+}
+
+#comp-jckajvku {
+    position: relative;
+    margin: 82px 0px 10px calc((100% - 980px) * 0.5);
+    left: 226px;
+    grid-area: 1 / 1 / 2 / 2;
+    justify-self: start;
+    align-self: start;
+}
+
+#comp-jckajvkv {
+    position: relative;
+    margin: 52px 0px 0px 0;
+    left: 255px;
+    grid-area: 1 / 1 / 2 / 2;
+    justify-self: start;
+    align-self: start;
+}
+
+#comp-jmstz2a3 {
+    position: relative;
+    margin: 82px 0px 10px calc((100% - 980px) * 0.5);
+    left: 564px;
+    grid-area: 1 / 1 / 2 / 2;
+    justify-self: start;
+    align-self: start;
+}
+
+#comp-j9sf5fvi {
+    position: relative;
+    margin: 0px 0px 0 calc((100% - 980px) * 0.5);
+    left: 0px;
+    grid-area: 1 / 1 / 2 / 2;
+    justify-self: start;
+    align-self: start;
+}
+
+#comp-jckajvjqinlineContent {
+    height: auto;
+    width: 218px;
+    position: relative;
+}
+
+#comp-jckajvkuinlineContent {
+    height: auto;
+    width: 338px;
+    position: relative;
+}
+
+#comp-jckajvjqinlineContent-gridContainer {
+    position: static;
+    height: auto;
+    width: 100%;
+    min-height: 93px;
+}
+
+#comp-jckajvkuinlineContent-gridContainer {
+    position: static;
+    display: grid;
+    height: auto;
+    width: 100%;
+    min-height: auto;
+    margin-bottom: -7px;
+    grid-template-rows: 1fr;
+    grid-template-columns: 100%;
+}
+
+#comp-jckajvkuinlineContent-gridWrapper {
+  display: flex;
+ pointer-events: none;
+}
+
+.txtNew {
+  word-wrap:break-word;
+  text-align:start;
+}
+.txtNew_override-left * {text-align:left !important;}
+.txtNew_override-right * {text-align:right !important;}
+.txtNew_override-center * {text-align:center !important;}
+.txtNew_override-justify * {text-align:justify !important;}
+.txtNew > * {pointer-events:auto;}
+.txtNew li {font-style:inherit;font-weight:inherit;line-height:inherit;letter-spacing:normal;}
+.txtNew ol,.txtNew ul {padding-left:1.3em;padding-right:0;margin-left:0.5em;margin-right:0;line-height:normal;letter-spacing:normal;}
+.txtNew ul {list-style-type:disc;}
+.txtNew ol {list-style-type:decimal;}
+.txtNew ul ul,.txtNew ol ul {list-style-type:circle;}
+.txtNew ul ul ul,.txtNew ol ul ul {list-style-type:square;}
+.txtNew ul ol ul,.txtNew ol ol ul {list-style-type:square;}
+.txtNew ul[dir="rtl"],.txtNew ol[dir="rtl"] {padding-left:0;padding-right:1.3em;margin-left:0;margin-right:0.5em;}
+.txtNew ul[dir="rtl"] ul,.txtNew ul[dir="rtl"] ol,.txtNew ol[dir="rtl"] ul,.txtNew ol[dir="rtl"] ol {padding-left:0;padding-right:1.3em;margin-left:0;margin-right:0.5em;}
+.txtNew p {margin:0;line-height:normal;letter-spacing:normal;}
+.txtNew h1 {margin:0;line-height:normal;letter-spacing:normal;}
+.txtNew h2 {margin:0;line-height:normal;letter-spacing:normal;}
+.txtNew h3 {margin:0;line-height:normal;letter-spacing:normal;}
+.txtNew h4 {margin:0;line-height:normal;letter-spacing:normal;}
+.txtNew h5 {margin:0;line-height:normal;letter-spacing:normal;}
+.txtNew h6 {margin:0;line-height:normal;letter-spacing:normal;}
+.txtNew a {color:inherit;}
+
+
+.white-box {
+  border:0px solid rgba(248, 246, 0, 1);
+  background-color:rgba(255, 255, 255, 1);
+  border-radius:0;  
+  position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+}
+
+.style-jomyp1cpinlineContent {
+  position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+}
+
+.style-jhkmf1umbg {
+  overflow:hidden;
+  position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+  background-color:rgba(252, 151, 121, 1);
+}
+
+.style-jckajvkw {
+  -webkit-tap-highlight-color:rgba(0, 0, 0, 0);
+}
+
+.style-jckajvkwlink,.style-jckajvkwsvg {position:absolute;top:0;right:0;bottom:0;left:0;}
+.style-jckajvkw svg {position:absolute;top:0;right:0;bottom:0;left:0;width:100%;height:100%;margin:auto;}
+.style-jckajvkw_non-scaling-stroke * {vector-effect:non-scaling-stroke;}
+.style-jckajvkw_with-shadow svg {overflow:visible !important;}
+
+.style-jhkmf1uminlineContent {
+  position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+}
+
+.style-jckajvkwsvg {
+  position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+}
+
+.style-jckajvkw_non-scaling-stroke * {vector-effect:non-scaling-stroke;}
+
+.font_2 {
+  font: normal normal normal 54px/1.4em anton,sans-serif;
+  color:#FFFFFF;
+}
+.color_11 {
+  color: #000000;
+}
+</style>
---
+++ SourceCode_and_Documentation/thelaunch/src/views/News.vue
@@ -0,0 +1,422 @@
+<template>
+  <div>
+    <div id="news-wrap">
+      <Header></Header>
+    </div>
+
+    <div class="container">
+      <div class="title">
+        <div>
+          <h1 id="big-title">
+            Latest News
+          </h1>
+        </div>
+      </div>
+
+      <div class="content">
+        <div class="index-news">
+          <div class="border-line"></div>
+          <div style="padding:20px;overflow:hidden;margin:0px 20%;">
+            <article>
+              <div class="news-title">
+                <h2
+                  class="title-font"
+                  style="inline-height:1.4em;font-size:26px;"
+                >
+                  <span style="font-size:26px;">News Title 01</span>
+                </h2>
+                <p class="date-font" style="font-size:12px; line-height=1.5em;">
+                  <span style="font-size:12px;">
+                    <span style="letter-spacing:0em;">MAR. 23, 2020</span>
+                  </span>
+                </p>
+              </div>
+              <div class="news-detail">
+                <p
+                  class="detail-font"
+                  style="font-size:14px;line-height:1.6em;"
+                >
+                  <span style="font-size:14px;">
+                    <span style="letter-spacing:0em;">
+                      I'm a paragraph. I'm connected to your collection through
+                      a dataset. Click Preview to see my content. To update me,
+                      go to the Data Manager.
+                    </span>
+                  </span>
+                </p>
+                <div>
+                  <a
+                    href="https://www.youtube.com/watch?v=cQAXMjxh04I"
+                    target="_self"
+                  >
+                    <span
+                      @mouseover="hover = true"
+                      @mouseleave="hover = false"
+                      :class="{ active: hover }"
+                      class="readmore-font"
+                      style="margin-left:0px;"
+                      >Read More</span
+                    >
+                  </a>
+                </div>
+              </div>
+              <div class="news-img"></div>
+            </article>
+          </div>
+
+          <div class="border-line"></div>
+          <div style="padding:20px;overflow:hidden;margin:0px 20%;">
+            <article>
+              <div class="news-title">
+                <h2
+                  class="title-font"
+                  style="inline-height:1.4em;font-size:26px;"
+                >
+                  <span style="font-size:26px;">News Title 02</span>
+                </h2>
+                <p class="date-font" style="font-size:12px; line-height=1.5em;">
+                  <span style="font-size:12px;">
+                    <span style="letter-spacing:0em;">MAR. 23, 2020</span>
+                  </span>
+                </p>
+              </div>
+              <div class="news-detail">
+                <p
+                  class="detail-font"
+                  style="font-size:14px;line-height:1.6em;"
+                >
+                  <span style="font-size:14px;">
+                    <span style="letter-spacing:0em;">
+                      I'm a paragraph. I'm connected to your collection through
+                      a dataset. Click Preview to see my content. To update me,
+                      go to the Data Manager.
+                    </span>
+                  </span>
+                </p>
+                <div>
+                  <a
+                    href="https://www.youtube.com/watch?v=cQAXMjxh04I"
+                    target="_self"
+                  >
+                    <span
+                      @mouseover="hover = true"
+                      @mouseleave="hover = false"
+                      :class="{ active: hover }"
+                      class="readmore-font"
+                      style="margin-left:0px;"
+                      >Read More</span
+                    >
+                  </a>
+                </div>
+              </div>
+              <div class="news-img"></div>
+            </article>
+          </div>
+
+          <div class="border-line"></div>
+          <div style="padding:20px;overflow:hidden;margin:0px 20%;">
+            <article>
+              <div class="news-title">
+                <h2
+                  class="title-font"
+                  style="inline-height:1.4em;font-size:26px;"
+                >
+                  <span style="font-size:26px;">News Title 03</span>
+                </h2>
+                <p class="date-font" style="font-size:12px; line-height=1.5em;">
+                  <span style="font-size:12px;">
+                    <span style="letter-spacing:0em;">MAR. 23, 2020</span>
+                  </span>
+                </p>
+              </div>
+              <div class="news-detail">
+                <p
+                  class="detail-font"
+                  style="font-size:14px;line-height:1.6em;"
+                >
+                  <span style="font-size:14px;">
+                    <span style="letter-spacing:0em;">
+                      I'm a paragraph. I'm connected to your collection through
+                      a dataset. Click Preview to see my content. To update me,
+                      go to the Data Manager.
+                    </span>
+                  </span>
+                </p>
+                <div>
+                  <a
+                    href="https://www.youtube.com/watch?v=cQAXMjxh04I"
+                    target="_self"
+                  >
+                    <span
+                      @mouseover="hover = true"
+                      @mouseleave="hover = false"
+                      :class="{ active: hover }"
+                      class="readmore-font"
+                      style="margin-left:0px;"
+                      >Read More</span
+                    >
+                  </a>
+                </div>
+              </div>
+              <div class="news-img"></div>
+            </article>
+          </div>
+
+          <div class="border-line"></div>
+          <div style="padding:20px;overflow:hidden;margin:0px 20%;">
+            <article>
+              <div class="news-title">
+                <h2
+                  class="title-font"
+                  style="inline-height:1.4em;font-size:26px;"
+                >
+                  <span style="font-size:26px;">News Title 04</span>
+                </h2>
+                <p class="date-font" style="font-size:12px; line-height=1.5em;">
+                  <span style="font-size:12px;">
+                    <span style="letter-spacing:0em;">MAR. 23, 2020</span>
+                  </span>
+                </p>
+              </div>
+              <div class="news-detail">
+                <p
+                  class="detail-font"
+                  style="font-size:14px;line-height:1.6em;"
+                >
+                  <span style="font-size:14px;">
+                    <span style="letter-spacing:0em;">
+                      I'm a paragraph. I'm connected to your collection through
+                      a dataset. Click Preview to see my content. To update me,
+                      go to the Data Manager.
+                    </span>
+                  </span>
+                </p>
+                <div>
+                  <a
+                    href="https://www.youtube.com/watch?v=cQAXMjxh04I"
+                    target="_self"
+                  >
+                    <span
+                      @mouseover="hover = true"
+                      @mouseleave="hover = false"
+                      :class="{ active: hover }"
+                      class="readmore-font"
+                      style="margin-left:0px;"
+                      >Read More</span
+                    >
+                  </a>
+                </div>
+              </div>
+              <div class="news-img"></div>
+            </article>
+          </div>
+
+          <div class="border-line"></div>
+          <div style="padding:20px;overflow:hidden;margin:0px 20%;">
+            <article>
+              <div class="news-title">
+                <h2
+                  class="title-font"
+                  style="inline-height:1.4em;font-size:26px;"
+                >
+                  <span style="font-size:26px;">News Title 05</span>
+                </h2>
+                <p class="date-font" style="font-size:12px; line-height=1.5em;">
+                  <span style="font-size:12px;">
+                    <span style="letter-spacing:0em;">MAR. 23, 2020</span>
+                  </span>
+                </p>
+              </div>
+              <div class="news-detail">
+                <p
+                  class="detail-font"
+                  style="font-size:14px;line-height:1.6em;"
+                >
+                  <span style="font-size:14px;">
+                    <span style="letter-spacing:0em;">
+                      I'm a paragraph. I'm connected to your collection through
+                      a dataset. Click Preview to see my content. To update me,
+                      go to the Data Manager.
+                    </span>
+                  </span>
+                </p>
+                <div>
+                  <a
+                    href="https://www.youtube.com/watch?v=cQAXMjxh04I"
+                    target="_self"
+                  >
+                    <span
+                      @mouseover="hover = true"
+                      @mouseleave="hover = false"
+                      :class="{ active: hover }"
+                      class="readmore-font"
+                      style="margin-left:0px;"
+                      >Read More</span
+                    >
+                  </a>
+                </div>
+              </div>
+              <div class="news-img"></div>
+            </article>
+          </div>
+
+          <div class="border-line"></div>
+          <div style="padding:20px;overflow:hidden;margin:0px 20%;">
+            <article>
+              <div class="news-title">
+                <h2
+                  class="title-font"
+                  style="inline-height:1.4em;font-size:26px;"
+                >
+                  <span style="font-size:26px;">News Title 06</span>
+                </h2>
+                <p class="date-font" style="font-size:12px; line-height=1.5em;">
+                  <span style="font-size:12px;">
+                    <span style="letter-spacing:0em;">MAR. 23, 2020</span>
+                  </span>
+                </p>
+              </div>
+              <div class="news-detail">
+                <p
+                  class="detail-font"
+                  style="font-size:14px;line-height:1.6em;"
+                >
+                  <span style="font-size:14px;">
+                    <span style="letter-spacing:0em;">
+                      I'm a paragraph. I'm connected to your collection through
+                      a dataset. Click Preview to see my content. To update me,
+                      go to the Data Manager.
+                    </span>
+                  </span>
+                </p>
+                <div>
+                  <a
+                    href="https://www.youtube.com/watch?v=cQAXMjxh04I"
+                    target="_self"
+                  >
+                    <span
+                      @mouseover="hover = true"
+                      @mouseleave="hover = false"
+                      :class="{ active: hover }"
+                      class="readmore-font"
+                      style="margin-left:0px;"
+                      >Read More</span
+                    >
+                  </a>
+                </div>
+              </div>
+              <div class="news-img"></div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="news-wrap">
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "News",
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      hover: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+// Your AWESOME styles go here
+#news-wrap {
+  background-color: black;
+}
+#big-title {
+  text-align: center;
+  font: normal normal normal 54px/1.4em dinneuzeitgroteskltw01-_812426,
+    sans-serif;
+  color: #fc9779;
+}
+// contact-middle
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 200px 1fr;
+  grid-template-areas:
+    "title"
+    "content";
+}
+.container > .title {
+  grid-area: title;
+  background-color: #000000;
+}
+.container > .content {
+  grid-area: content;
+  background-color: #a0f;
+}
+.news-title {
+  //border: solid 1px; //delete
+  float: left;
+  width: 30%;
+}
+.title-font {
+  font: normal normal normal 54px/1.4em anton, sans-serif;
+  color: #ffffff;
+}
+.date-font {
+  font: normal normal bold 14px/1.4em avenir-lt-w01_35-light1475496, sans-serif;
+  color: #ffffff;
+}
+.news-detail {
+  //border: solid 1px; //delete
+  float: left;
+  width: 36%;
+  margin: 0px 3%;
+}
+.detail-font {
+  font: normal normal bold 14px/1.4em avenir-lt-w01_35-light1475496, sans-serif;
+  color: #ffffff;
+}
+.readmore-font {
+  font: normal normal 700 14px/1.4em poppins-extralight, poppins, sans-serif;
+  transition: color 0.4s ease 0s;
+  color: #fc9779;
+  white-space: nowrap;
+  display: inline-block;
+}
+.active {
+  color: #7e4c3d;
+  transition: color 0.4s ease 0s;
+}
+.news-img {
+  //border: solid 1px; //delete
+  float: left;
+  width: 28%;
+  height: 120px;
+  background-color: rgb(235, 190, 191);
+  /*background-image: url('');*/
+}
+.border-line {
+  margin: 0px 20%;
+  box-sizing: border-box;
+  border-top: 1px solid rgba(27, 27, 27, 1);
+  height: 0;
+}
+.index-news {
+  width: 100%;
+  padding: 80px 0px;
+  background-color: #000;
+  overflow: hidden;
+}
+</style>
