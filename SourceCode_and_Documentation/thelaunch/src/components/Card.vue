55a6dec9e018e370b2aa08be46ab4b0bfbf8d26f
Sijia Hao <oh0917ce@gmail.com>
2020-04-22 01:40:25 +1000

    build-feat(Artists):finished artists page

---
+++ SourceCode_and_Documentation/thelaunch/src/components/Card.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="card-wrap">
+    <div class="content-wrap">
+      <div class="picture">
+        <img :src="url" width="204px" />
+      </div>
+      <div class="name-block">
+        <span>{{ name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    id: Number,
+  },
+  data() {
+    return {
+      url: require("../assets/artists/img/" + this.id + ".jpg"),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#card-wrap {
+  width: 224px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .content-wrap {
+    margin: 10px;
+    width: 204px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: transparent;
+    .name-block {
+      margin-top: -4px;
+      width: 100%;
+      height: 55px;
+      background-color: #fff;
+      text-align: end;
+      line-height: 55px;
+      color: black;
+      font-size: 22px;
+      font-family: "Impact-W02-2010", sans-serif;
+      span {
+        margin-right: 30px;
+      }
+    }
+  }
+}
+</style>
--- SourceCode_and_Documentation/thelaunch/vue.config.js
+++ SourceCode_and_Documentation/thelaunch/vue.config.js
@@ -1,7 +1,17 @@
+var artists = require("./src/assets/artists/data.json"); //本地json文件数据
+
 module.exports = {
   productionSourceMap: false,
 
   devServer: {
+    before(app) {
+      app.get("/api/artists", (req, res) => {
+        res.json({
+          errno: 0, // 这里是你的json内容
+          data: artists,
+        });
+      });
+    },
     open: process.platform === "darwin",
     host: "0.0.0.0",
     port: 8080,
